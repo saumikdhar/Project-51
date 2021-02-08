281d0018611d44ffd450afc7ed5d1e2f9e940dd8
Saumik Dhar <dhars@cardiff.ac.uk>
2021-02-08 02:39:32 +0000

    implement login form

--- project-management-frontend/src/Containers/Auth/Auth.js
+++ project-management-frontend/src/Containers/Auth/Auth.js
@@ -1,26 +1,57 @@
 import React, {useState} from "react";
-import {connect} from "react-redux";
-import {Redirect} from "react-router-dom";
 import classes from "./Auth.module.css";
 import Auxiliary from "../../hoc/Auxiliary/Auxiliary";
+import Button from "../../Components/UI/Button/Button";
+import Input from "../../Components/UI/Input/Input";
 
 const Auth = (props) => {
-    const [userForm, setUserForm] = useState({
-        email: "",
-        password: ""
+    const [authForm, setAuthForm] = useState({
+        email:  {
+            type: "email",
+            value: ""
+        },
+        password:{
+            type: "password",
+            value: ""
+        }
     })
+
+    const formElementsArray = [];
+    for (let key in authForm) {
+        formElementsArray.push({
+            id: key,
+            config: authForm[key]
+        });
+    }
+
+    let form = formElementsArray.map(formElement => {
+        return (
+          <Input
+            style={{display: 'none'}}
+            key={formElement.id}
+            elementType="input"
+            elementConfig={formElement.config.elementConfig}
+            value={formElement.config.value}
+            // changed={event => inputChangeHandler(event, formElement.id)}
+          />
+        );
+    });
 
     const submitHandler = event => {
         event.preventDefault();
     };
 
     return (
-        <Auxiliary>
-            <div className={classes.Auth}>
-                <h1>Login page</h1>
-            </div>
-        </Auxiliary>
+      <Auxiliary>
+          <div className={classes.Auth}>
+              <h1>Sign in</h1>
+              <form onSubmit={submitHandler}>
+                  {form}
+                  <Button btnType="Submit">Submit</Button>
+              </form>
+          </div>
+      </Auxiliary>
     );
 }
 
-export default (Auth);
+export default Auth;
