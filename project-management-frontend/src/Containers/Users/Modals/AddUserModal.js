a4dd947d49de3415a411d5cdc4cb7f1b20b0262d
Saumik Dhar <dhars@cardiff.ac.uk>
2021-02-28 12:47:16 +0000

    Assign projects

--- project-management-frontend/src/Containers/Users/Modals/AddUserModal.js
+++ project-management-frontend/src/Containers/Users/Modals/AddUserModal.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import {connect} from "react-redux";
-import {Button, Form, Input, Modal, Select} from "antd";
-import {PhoneOutlined, MailOutlined, LockOutlined} from '@ant-design/icons';
-import {addUser} from "../../../store/actions/index";
+import { connect } from 'react-redux';
+import { Button, Form, Input, Modal, Select } from 'antd';
+import { PhoneOutlined, MailOutlined, LockOutlined } from '@ant-design/icons';
+import { addUser } from '../../../store/actions/index';
 
 const FormItem = Form.Item;
 const Option = Select.Option;
 
 class AddUserModal extends React.Component {
-
   onSubmit = values => {
-    values["projectId"] = this.props.projectId;
+    values['projectId'] = this.props.projectId;
     this.props.onSubmit(values);
     this.props.hideModal();
   };
 
   refreshUsers = () => this.props.refreshUsers();
 
-
   render() {
-    const {visible, hideModal} = this.props;
+    const { visible, hideModal } = this.props;
 
     return (
       <Modal
@@ -29,88 +27,103 @@ class AddUserModal extends React.Component {
         destroyOnClose={true}
         visible={visible}
         footer={[
-          <Button key="Cancel" onClick={hideModal}>Cancel</Button>,
-          <Button key="submit" type="primary" form="AddUserForm" htmlType="submit">{"Add"}</Button>
-        ]}>
-        <Form id="AddUserForm" layout='vertical' onFinish={this.onSubmit}>
+          <Button key="Cancel" onClick={hideModal}>
+            Cancel
+          </Button>,
+          <Button key="submit" type="primary" form="AddUserForm" htmlType="submit">
+            {'Add'}
+          </Button>
+        ]}
+      >
+        <Form id="AddUserForm" layout="vertical" onFinish={this.onSubmit}>
           <FormItem
             label="First Name"
             name="firstName"
-            rules={[{
-              whitespace: true,
-              required: true,
-              message: 'Please enter the new user\'s first name',
-            }]}
+            rules={[
+              {
+                whitespace: true,
+                required: true,
+                message: "Please enter the new user's first name"
+              }
+            ]}
           >
-            <Input placeholder="Ex. Wesley"/>
+            <Input placeholder="Ex. Wesley" />
           </FormItem>
 
           <FormItem
             label="Surname"
             name="surname"
-            rules={[{
-              whitespace: true,
-              required: true,
-              message: 'Please enter the user\'s surname',
-            }]}
+            rules={[
+              {
+                whitespace: true,
+                required: true,
+                message: "Please enter the user's surname"
+              }
+            ]}
           >
-            <Input placeholder="Ex. Snipes"/>
+            <Input placeholder="Ex. Snipes" />
           </FormItem>
 
           <FormItem
             label="Email"
             name="email"
             rules={[
-              {required: true, message: 'Please enter the new user\'s email'},
+              { required: true, message: "Please enter the new user's email" },
               {
                 pattern: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
                 message: 'Sorry, please enter a valid email'
               }
             ]}
           >
-            <Input prefix={<MailOutlined />} placeholder="Ex. Wesley.Snipes@gmail.com"/>
+            <Input prefix={<MailOutlined />} placeholder="Ex. Wesley.Snipes@gmail.com" />
           </FormItem>
 
           <FormItem
             label="Password"
             name="password"
-            rules={[
-              {required: true, message: 'Please enter the new user\'s password'}
-            ]}
+            rules={[{ required: true, message: "Please enter the new user's password" }]}
           >
-            <Input prefix={<LockOutlined />} placeholder="The User's Password"/>
+            <Input prefix={<LockOutlined />} placeholder="The User's Password" />
           </FormItem>
 
           {/*<FormItem*/}
-            {/*label="Phone Number"*/}
-            {/*name="phone"*/}
-            {/*rules={[{*/}
-              {/*whitespace: true,*/}
-              {/*required: false,*/}
-              {/*message: "Please enter a valid email"*/}
-            {/*},*/}
-              {/*{*/}
-                {/*pattern: /^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,3})|(\(?\d{2,3}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$/,*/}
-                {/*message: 'Invalid phone number'*/}
-              {/*}*/}
-            {/*]}*/}
+          {/*label="Phone Number"*/}
+          {/*name="phone"*/}
+          {/*rules={[{*/}
+          {/*whitespace: true,*/}
+          {/*required: false,*/}
+          {/*message: "Please enter a valid email"*/}
+          {/*},*/}
+          {/*{*/}
+          {/*pattern: /^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,3})|(\(?\d{2,3}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$/,*/}
+          {/*message: 'Invalid phone number'*/}
+          {/*}*/}
+          {/*]}*/}
           {/*>*/}
-            {/*<Input prefix={<PhoneOutlined/>} placeholder="Ex. +44 7502719493"/>*/}
+          {/*<Input prefix={<PhoneOutlined/>} placeholder="Ex. +44 7502719493"/>*/}
           {/*</FormItem>*/}
-
 
           <FormItem
             label="Role"
             extra="The role defines what permissions the user will have"
             name="role"
-            rules={[{
-              required: true,
-              message: 'Please select a user role',
-            }]}
+            rules={[
+              {
+                required: true,
+                message: 'Please select a user role'
+              }
+            ]}
           >
             <Select>
-              <Option key={"ProjectManager"} value={"ProjectManager"}>{"Project Manager"}</Option>
-              <Option key={"employee"} value={"employee"}>{"Employee"}</Option>
+              <Option key={'ProjectManager'} value={'projectManager'}>
+                {'Project Manager'}
+              </Option>
+              <Option key={'employee'} value={'employee'}>
+                {'Employee'}
+              </Option>
+              <Option key={'TransformationTeam'} value={'transformationTeam'}>
+                {'Transformation Team'}
+              </Option>
             </Select>
           </FormItem>
         </Form>
@@ -119,4 +132,4 @@ class AddUserModal extends React.Component {
   }
 }
 
-export default (AddUserModal)
+export default AddUserModal;
