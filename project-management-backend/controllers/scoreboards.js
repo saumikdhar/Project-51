91cbaf0c11c3c3e24ff42f1791a5de63398cc233
Saumik Dhar <dhars@cardiff.ac.uk>
2021-03-16 18:15:29 +0000

    Add RAG chart to scoreboard

--- project-management-backend/controllers/scoreboards.js
+++ project-management-backend/controllers/scoreboards.js
@@ -1,16 +1,27 @@
-const Project = require("../models/project");
-const Scoreboard = require("../models/scoreboard");
+const Project = require('../models/project');
+const Scoreboard = require('../models/scoreboard');
 
-exports.getScoreboard= async(req,res,next)=>{
-    const projectId=req.params.id
-    const scoreboard=await Scoreboard.findAll({
-        where:{
-            projectId :projectId
-        }
-    })
-console.log('scoreboard',scoreboard)
+exports.getScoreboard = async (req, res, next) => {
+  const projectId = req.body.projectId;
+  try {
+    const scoreboard = await Scoreboard.findAll({
+      where: {
+        projectId: projectId
+      }
+    });
+    if (!scoreboard) {
+      const error = new Error('Scoreboard not found!');
+      error.statusCode = 404;
+      throw error;
+    }
     res.status(200).json({
-        success:true,
-        data:scoreboard
-    })
-}
+      success: true,
+      data: scoreboard
+    });
+  } catch (error) {
+    if (!error.statusCode) {
+      error.statusCode = 500;
+    }
+    next(error);
+  }
+};
