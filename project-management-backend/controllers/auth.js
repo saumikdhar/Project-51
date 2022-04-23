8a43f0fe3ce2dbe954a46d38ad0b7a033121a0c9
Saumik <dhars@cardiff.ac.uk>
2022-04-23 22:26:03 +0100

    adding files

--- project-management-backend/controllers/auth.js
+++ project-management-backend/controllers/auth.js
@@ -4,6 +4,46 @@ const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
+exports.signup = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation failed.');
+    error.statusCode = 422;
+    const email = req.body.email;
+    const name = req.body.name;
+    const password = req.body.password;
+    console.log(email, password, name);
+    res.status(422).json({ error: error });
+    return;
+  }
+  const email = req.body.email;
+  const name = req.body.name;
+  const password = req.body.password;
+  try {
+    const hashedPw = await bcrypt.hash(password, 12);
+    const user = new User({
+      email: email,
+      password: hashedPw,
+      name: name
+    });
+    const result = await user.save();
+    const token = jwt.sign(
+      {
+        email: result.email,
+        userId: result._id.toString()
+      },
+      'somesupersecretsecret',
+      { expiresIn: '1h' }
+    );
+    res.status(201).json({ token: token, message: 'User created!', userId: result._id });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
+
 exports.login = async (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
